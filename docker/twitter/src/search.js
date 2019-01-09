409c3ffe7df56eae691caca67cee9af02413717b
JeffBoulot <jeffgazet@gmail.com>
2019-01-09 17:34:30 +0100

    Fix since_id param

--- docker/twitter/src/search.js
+++ docker/twitter/src/search.js
@@ -21,13 +21,12 @@ let t = new Twit({
     app_only_auth: true // app auth is limit 450/15', user auth is 180/15'
     });
 
-let since_id = '';
-const params = {
+let params = {
     q: process.env.SEARCH_REST_q,
     result_type: 'recent',
     count: 100,
     lang: process.env.SEARCH_REST_lang,
-    since_id: since_id
+    since_id: ''
     };
 
 let search = function() {
@@ -38,7 +37,7 @@ let search = function() {
         let i = len-1;
         while(i >=0) {
             mysave.saveTweet(tweets.statuses[i]);
-            since_id = tweets.statuses[i].id_str; // next search after this last tweet id got
+            params.since_id = tweets.statuses[i].id_str; // next search after this last tweet id got
             i--;
             }
         console.log(len + ' tweets got, sleep ' + (SLEEP/1000) + 's');
